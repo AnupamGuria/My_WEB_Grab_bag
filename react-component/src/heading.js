117fc393fb611a5dda24443923de7d28ac7a0ac0
Anupam Guria <62615296+AnupamGuria@users.noreply.github.com>
2023-11-14 01:06:19 +0530

    Add files via upload
    Simple example of react component

---
+++ react-component/src/heading.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Heading(){
+    return(
+        <div>
+            <h1>Using React Component</h1>
+        </div>
+    )
+}
+export default Heading
---
+++ react-component/src/list.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function List(){
+    return(
+        <ul>
+            <li>Makes code more readable</li>
+            <li>Easy to test</li>
+            <li>Yields better performance</li>
+            <li>Debugging is a piece of cake</li>
+            <li>Reducing Coupling factor</li>
+        </ul>
+    )
+}
+
+export default List
